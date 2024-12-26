09d675d5e7783aaf546d9972c46e6d147045be65
shreyansh-1206 <s.shreyansh.1206@gmail.com>
2024-12-26 18:40:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+ function data(){
+var a= document.getElementById("1").value;
+var b= document.getElementById("2").value;
+var c= document.getElementById("3").value;
+if(a==""||b==""||c=="")
+{
+    alert("check if all fields are true");
+    return false;
+}
+else{
+    true;
+}
+
+
+
+}
